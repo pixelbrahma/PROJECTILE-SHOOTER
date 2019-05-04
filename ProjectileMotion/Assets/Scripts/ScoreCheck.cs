757aec62b1d1a860718096d2bec7541ffa32f6e7
Nikhil Bharadwaj <44049892+pixelbrahma@users.noreply.github.com>
2019-05-04 13:54:59 +0530

    adds

---
+++ ProjectileMotion/Assets/Scripts/ScoreCheck.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ScoreCheck : MonoBehaviour {
+
+    public static string text;
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        text = " SCORED !! ";
+        Invoke("Change", 5f);
+        Destroy(collision.gameObject);
+    }
+
+    void Change()
+    {
+        text = "";
+    }
+}
---
+++ ProjectileMotion/Assets/Scripts/Shooter.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Shooter : MonoBehaviour {
+
+    [SerializeField] private Transform shotPrefab;
+    private string powerText;
+    public static Vector2 shotDirection;
+    public static float power = 0f;
+    public Transform shot;
+
+    private void Start()
+    {
+        powerText = " ";
+    }
+
+    private void Update()
+    {
+        Vector2 mousePos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+        shotDirection = mousePos - new Vector2(transform.position.x,transform.position.y).normalized;
+        transform.up = shotDirection;
+        if(Input.GetKey(KeyCode.Space))
+        {
+            power += 0.05f;
+        }
+        if(Input.GetKeyUp(KeyCode.Space))
+        {
+            shot = Instantiate(shotPrefab, transform.position, Quaternion.identity);
+            Rigidbody2D rbb = shot.gameObject.GetComponent<Rigidbody2D>();
+            rbb.AddForce(Shooter.shotDirection * Shooter.power);
+            power = 0f;
+        }
+        powerText = " POWER : " + power;
+    }
+
+    private void OnGUI()
+    {
+        GUI.Label(new Rect(50, 20, 200, 100), powerText);
+        GUI.Label(new Rect(300, 20, 200, 100), ScoreCheck.text);
+    }
+}
